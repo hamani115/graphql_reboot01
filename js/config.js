5c16a6e1b12771935c73d6e2df4b32fb1ea5af51
hamani115 <abdulrahman.almarzouqi@cern.ch>
2026-04-16 23:02:25 +0300

    cleanup

--- js/config.js
+++ js/config.js
@@ -2,8 +2,3 @@ const CONFIG = {
     API_URL: 'https://learn.reboot01.com/api/graphql-engine/v1/graphql',
     AUTH_URL: 'https://learn.reboot01.com/api/auth/signin',
 };
-
-if (typeof process !== 'undefined' && process.env) {
-    CONFIG.API_URL = process.env.REACT_APP_API_URL || CONFIG.API_URL;
-    CONFIG.AUTH_URL = process.env.REACT_APP_AUTH_URL || CONFIG.AUTH_URL;
-}
